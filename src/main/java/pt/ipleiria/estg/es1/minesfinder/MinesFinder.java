2cbf34a1f25dc82809b0819a46daabe41fa09a33
Ana Martins <2201711@my.ipleiria.pt>
2022-03-09 10:22:48 +0000

    adicionar elemtnos Ãao painel

--- src/main/java/pt/ipleiria/estg/es1/minesfinder/MinesFinder.java
+++ src/main/java/pt/ipleiria/estg/es1/minesfinder/MinesFinder.java
@@ -4,10 +4,21 @@ import javax.swing.*;
 
 public class MinesFinder extends JFrame{
 
+    private JPanel painelPrincipal;
+    private JButton jogoFácilButton;
+    private JButton jogoMédioButton;
+    private JButton jogoDifícilButton1;
+    private JButton sairButton;
+    private JLabel northLabel;
+    private JPanel westJPanel;
+    private JPanel centerPanel;
+
     public MinesFinder(String title){
         super(title);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelPrincipal);
+        pack();
     }
 
     public static void main(String[] args) {
