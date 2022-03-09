426c907ae37d59bb0c6052b3d839a8da8f52dea3
Ana Martins <2201711@my.ipleiria.pt>
2022-03-09 19:09:28 +0000

    Aspeto Visual

--- src/main/java/pt/ipleiria/estg/es1/minesfinder/MinesFinder.java
+++ src/main/java/pt/ipleiria/estg/es1/minesfinder/MinesFinder.java
@@ -1,14 +1,15 @@
 package pt.ipleiria.estg.es1.minesfinder;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class MinesFinder extends JFrame{
 
     private JPanel painelPrincipal;
     private JButton jogoFácilButton;
     private JButton jogoMédioButton;
-    private JButton jogoDifícilButton1;
-    private JButton sairButton;
+    private JButton jogoDifícilButton;
+    private JButton btnSair;
     private JLabel northLabel;
     private JPanel westJPanel;
     private JPanel centerPanel;
@@ -19,6 +20,11 @@ public class MinesFinder extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(painelPrincipal);
         pack();
+        btnSair.addActionListener(this::btnSairActionPerformed);
+    }
+
+    private void btnSairActionPerformed(ActionEvent e) {
+        System.exit(0);
     }
 
     public static void main(String[] args) {
